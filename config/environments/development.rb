d9dd4d454ef678ff86ea822f5d6dca95d50e270d
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2022-12-30 14:29:09 +0000

    Update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -71,6 +71,9 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.active_job.queue_adapter = :inline
+
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << '422f3abef127440ea144a5818debabb8.vfs.cloud9.ap-northeast-1.amazonaws.com'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :customers
+  get 'homes/top'
+  root to: 'homes#top'
 end
