f80627ac2e203dd37ca3d95e84810fa46150b3c0
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2023-01-29 07:19:45 +0000

    Search実装

--- app/controllers/public/movies_controller.rb
+++ app/controllers/public/movies_controller.rb
@@ -4,16 +4,18 @@ class Public::MoviesController < ApplicationController
   Tmdb::Api.key("0710fca52ffb53cb379f38e8f53fcce6")
   Tmdb::Api.language("ja") # こちらで映画情報の表示の際の言語設定を日本語にできます
 
-  def search
-  end
+  # def search
+  # end
 
   def show
-    moviedata = Tmdb::Collection.detail(10)
-    pp moviedata
+    @movie = Tmdb::Movie.detail(params[:id])
+    # @movie = Tmdb::Search.movie()
+    # moviedata = Tmdb::Collection.detail(10)
+    # pp moviedata
   end
 
   def index
-    moviedata = Tmdb::Search.movie("Harry")
-    pp moviedata
+    @moviedata = Tmdb::Search.movie("Harry")
+    pp @moviedata
   end
 end
