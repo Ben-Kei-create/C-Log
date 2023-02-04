2923d82a014bd3933bf688be4b0055a25f4df4ed
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2023-02-04 08:03:21 +0000

    いいね機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,12 @@ devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
-
 scope module: :public do
   root to: "homes#top"
-  #どちらが必要か？？↓createの部分
   resources :movies, only: [:index, :show, :create, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:new, :create, :index]
   end
-
   get 'movies/search' => 'movies#search'
   get 'movies/:id/review' => 'movies#review', as: 'review'
   get 'homes/about' => 'homes#about', as: 'about'
@@ -23,29 +20,15 @@ scope module: :public do
   get 'customers/unsubscribe' => 'customers#unsubscribe'
   resources :customers, only: [:show, :edit, :update]
   patch '/customers/:id/withdrawal' => 'customers#withdrawal', as: 'withdrawal'
-# # # delete '/cart_items/destroy_all' => 'cart_items#destroy_all', as: 'cart_items_destroy_all'
-# # # resources :cart_items, only: [:index, :destroy, :create, :update]
-# # # get 'orders/complete' => 'orders#complete'
-# # # resources :orders, only: [:new, :create, :index, :show]
-# # # resources :addresses, only: [:new, :index, :show, :create, :update, :destroy, :edit]
-# # # get 'customers/information' => 'customers#show'
-# # # get 'orders/confirm' => 'orders#confirm'
-# # # post 'orders/confirm' => 'orders#confirm'
-# # # post 'customers/information' => 'customers#new'
 end
-
-
-
 
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
-
-
 namespace :admin do
   root to: "homes#top"
   resources :messages, only: [:new, :create, :show, :edit, :update, :destroy]
-# # resources :customers, only: [:index, :show, :edit, :update]
+  resources :customers, only: [:index]
 # # resources :genres, only: [:index, :create, :edit, :update]
 # # patch 'order_details/:id' => 'order_details#update'
 # # resources :orders, only: [:show, :edit, :update]
