72911007286b942e8b22d9d67dd86246bdf96afd
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2023-02-05 02:57:03 +0000

    鑑賞したフォロー機能実装中

--- app/controllers/public/watches_controller.rb
+++ app/controllers/public/watches_controller.rb
@@ -1,18 +1,20 @@
 class Public::WatchesController < ApplicationController
 
   def create
-  @post_Watched= Watch.new(customer_id: current_customer.id, movie_id: params[:movie_id])
-  @post_Watched.save
-  redirect_to movie_path(params[:movie_id])
+    @post_Watched = Watch.new(customer_id: current_customer.id, movie_id: params[:movie_id])
+    @post_Watched.save
+    redirect_to movie_path(params[:movie_id])
   end
 
   def destroy
-  @post_Watched = Watch.find_by(customer_id: current_customer.id, movie_id: params[:movie_id])
-  @post_Watched.destroy
-  redirect_to movie_path(params[:movie_id])
+    @post_Watched = Watch.find_by(customer_id: current_customer.id, movie_id: params[:movie_id])
+    @post_Watched.destroy
+    redirect_to movie_path(params[:movie_id])
   end
 
   def index
+    @watch = Watch.all
+    @watch = Watch.where(customer_id: current_customer.id, movie_id: params[:movie_id])
+    @watch = Watch.page(params[:page]).per(3)
   end
-
 end
