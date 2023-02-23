a28bc26c4c1639fdeabe2e2baf5f91c6201addf2
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2023-02-23 10:33:01 +0000

    ログアウト機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # before_action :authenticate_admin!
-
-  # def after_sign_in_path_for(resource)
-  #   admin_root_path # ログイン後に遷移するpathを設定
-  # end
-
-  # def after_sign_out_path_for(resource)
-  #   new_admin_session_path # ログアウト後に遷移するpathを設定
-  # end
+  def after_sign_out_path_for(resource)
+    if resource == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
+  end
 
   protected
   def configure_permitted_parameters
--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,9 +1,9 @@
 class Public::HomesController < ApplicationController
 
   def top
-    @messages = Message.limit(4).order('id DESC')
+    @messages = Message.limit(3).order('id DESC')
     @impressions = Impression.where(created_at: (Time.zone.today.beginning_of_day - 30.day)..Time.zone.today.end_of_day)
-    @impressions = @impressions.limit(4).order('id DESC')
+    @impressions = @impressions.limit(3).order('id DESC')
   end
 
   def about
